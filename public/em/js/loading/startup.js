56b603005d64ceef49913b3387e1f2d11ed404e0
lerry <johnlerryramossamson@gmail.com>
2025-02-19 13:30:27 +0800

    Debug

--- public/em/js/loading/startup.js
+++ public/em/js/loading/startup.js
@@ -2,6 +2,8 @@ const loadingBackground = document.querySelector('.loading-background');
 const offset = innerHeight * 0.5; // 50%                                   50% * -1
 const pageTop = Math.abs(sessionStorage.getItem('page-top')) - offset || (offset * -1); // subtract 50% since this loading container or background has 200vh of height to prevent displaying the behind from unexpected pushing downward of this loading container
 
+console.log('test');
+
 loadingBackground.style.top = `${pageTop}px`;
 document.body.style.overflow = 'hidden';
 document.addEventListener('keydown', disableKeyBoardAndScroll);
