ee20217ae9ca208e23c23ce26c36855a89fbb49a
John Lerry Ramos Samson <johnlerryramossamson@gmail.com>
2025-12-07 00:13:09 +0800

    Update the routes

--- index.js
+++ index.js
@@ -18,10 +18,9 @@ const connectDB = async () => {
 }
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-app.use('/js', express.static(path.join(__dirname, 'public/em/js')));
-app.use(express.static(path.join(__dirname, 'public/em/')));
-app.use(express.static(path.join(__dirname, 'public/em/blogs/node-server-min-setup.html')));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public/em')))
 
 // backend request
 const useRoute = require('./routes/messageRoutes');
